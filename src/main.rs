e99409a1de1afd328beb98d0101be3c8682de8f6
Ihar Novik <i.novik@godeltech.com>
2020-05-25 02:48:55 +0300

    Add start logic.

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,17 @@
 extern crate clap;
 
 use clap::{Arg, App, SubCommand};
-use std::env::args;
 
 fn main() {
     let arg_matches = App::new("kafka-cli-helper")
         .version("1.0")
-        .about("Kafka cli tools helper")
-        .arg(Arg::with_name("config")
-            .short("c")
-            .long("config")
-            .value_name("FILE")
-            .help("Sets a custom config file")
-            .takes_value(true))
-        .arg(Arg::with_name("INPUT")
-            .help("Sets the input file to use")
-            .required(true)
-            .index(1))
+        .about("Kafka cli helper")
+        .arg(Arg::with_name("ip")
+            .short("ip")
+            .long("kafka ip, localhost by default")
+            .value_name("ip")
+            .help("Sets kafka ip with port")
+            .default_value("localhost"))
         .subcommand(SubCommand::with_name("operation")
             .about("operation testing features")
             .arg(Arg::with_name("debug")
@@ -26,4 +21,7 @@ fn main() {
 
 //    CLI logic
     println!("argument matches: {:?}", arg_matches);
+    if let Some(ip) = arg_matches.value_of("ip") {
+        println!("Some kafka ip: {}", ip);
+    }
 }
