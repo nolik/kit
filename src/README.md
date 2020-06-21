ff1f2bcd12b162358c68a82e6ca5ecf77c0a708a
Ihar Novik <i.novik@godeltech.com>
2020-06-22 01:25:04 +0300

    Add topic subscription command / As well was added tokio with asynch futures.

--- src/README.md
+++ src/README.md
@@ -1,3 +1,3 @@
 kcli is a simple Kafka cli helper not require jvm.
 
-for local argument passing through cargo use: cargo run --color=always --package kcli --bin kcli -- -b 192.168.1.1:9092
+for local argument passing through cargo use: cargo run --color=always --package kcli --bin kcli -- -b localhost:9092
--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,121 @@
 extern crate clap;
 
-use clap::{Arg, App, SubCommand};
+use clap::{App, Arg, SubCommand};
+use log::{info, warn};
+use futures::StreamExt;
+use uuid::Uuid;
 
-fn main() {
-    let arg_matches = App::new("kcli")
+use rdkafka::client::ClientContext;
+use rdkafka::config::{ClientConfig, RDKafkaLogLevel};
+use rdkafka::consumer::stream_consumer::StreamConsumer;
+use rdkafka::consumer::{CommitMode, Consumer, ConsumerContext, Rebalance};
+use rdkafka::error::KafkaResult;
+use rdkafka::message::{Headers, Message};
+use rdkafka::topic_partition_list::TopicPartitionList;
+
+#[tokio::main]
+async fn main() {
+    let matches = App::new("kcli")
         .version("1.0")
         .about("Kafka cli helper")
-        .arg(Arg::with_name("broker")
-            .short("b")
-            .long("broker ip, localhost by default")
-            .value_name("b")
-            .help("Sets kafka broker ip with port")
-            .default_value("localhost:9092"))
-        .subcommand(SubCommand::with_name("operation")
-            .about("operation testing features")
-            .arg(Arg::with_name("consume")
-                .short("c")
-                .help("consume msg's from topic (eager by default)")))
+        .arg(
+            Arg::with_name("broker")
+                .short("b")
+                .long("brokers")
+                .help("Sets kafka broker ip with port")
+                .takes_value(true)
+                .default_value("localhost:9092"),
+        )
+        .subcommand(
+            SubCommand::with_name("operation")
+                .about("operation testing features")
+                .arg(
+                    Arg::with_name("consume_topic")
+                        .short("c")
+                        .help("consume msg's from topic (eager by default, 'test default topic)")
+                        .takes_value(true)
+                        .default_value("test"),
+                ),
+        )
         .get_matches();
 
-//    CLI logic
-    println!("argument matches: {:?}", arg_matches);
-    if let Some(broker) = arg_matches.value_of("broker") {
-        println!("Some kafka broker ip: {}", broker);
+    //    CLI logic
+    let broker = matches.value_of("broker").unwrap();
+    let rand_group_id = Uuid::new_v4().to_string() + "_kcli";
+    let topics = matches.values_of("consume_topic").unwrap().collect::<Vec<&str>>();
+
+    consume_and_print(broker, rand_group_id.as_ref(), &topics).await
+}
+
+// A context can be used to change the behavior of producers and consumers by adding callbacks
+// that will be executed by librdkafka.
+// This particular context sets up custom callbacks to log rebalancing events.
+struct CustomContext;
+
+impl ClientContext for CustomContext {}
+
+impl ConsumerContext for CustomContext {
+    fn pre_rebalance(&self, rebalance: &Rebalance) {
+        info!("Pre rebalance {:?}", rebalance);
+    }
+
+    fn post_rebalance(&self, rebalance: &Rebalance) {
+        info!("Post rebalance {:?}", rebalance);
+    }
+
+    fn commit_callback(&self, result: KafkaResult<()>, _offsets: &TopicPartitionList) {
+        info!("Committing offsets: {:?}", result);
+    }
+}
+
+// A type alias with your custom consumer can be created for convenience.
+type LoggingConsumer = StreamConsumer<CustomContext>;
+
+async fn consume_and_print(brokers: &str, group_id: &str, topics: &[&str]) {
+    let context = CustomContext;
+
+    let consumer: LoggingConsumer = ClientConfig::new()
+        .set("group.id", group_id)
+        .set("bootstrap.servers", brokers)
+        .set("enable.partition.eof", "false")
+        .set("session.timeout.ms", "6000")
+        .set("enable.auto.commit", "true")
+        //.set("statistics.interval.ms", "30000")
+        //.set("auto.offset.reset", "smallest")
+        .set_log_level(RDKafkaLogLevel::Debug)
+        .create_with_context(context)
+        .expect("Consumer creation failed");
+
+    consumer
+        .subscribe(&topics.to_vec())
+        .expect("Can't subscribe to specified topics");
+
+    // consumer.start() returns a stream. The stream can be used ot chain together expensive steps,
+    // such as complex computations on a thread pool or asynchronous IO.
+    let mut message_stream = consumer.start();
+
+    while let Some(message) = message_stream.next().await {
+        match message {
+            Err(e) => warn!("Kafka error: {}", e),
+            Ok(m) => {
+                let payload = match m.payload_view::<str>() {
+                    None => "",
+                    Some(Ok(s)) => s,
+                    Some(Err(e)) => {
+                        warn!("Error while deserializing message payload: {:?}", e);
+                        ""
+                    }
+                };
+                info!("key: '{:?}', payload: '{}', topic: {}, partition: {}, offset: {}, timestamp: {:?}",
+                      m.key(), payload, m.topic(), m.partition(), m.offset(), m.timestamp());
+                if let Some(headers) = m.headers() {
+                    for i in 0..headers.count() {
+                        let header = headers.get(i).unwrap();
+                        info!("  Header {:#?}: {:?}", header.0, header.1);
+                    }
+                }
+                consumer.commit_message(&m, CommitMode::Async).unwrap();
+            }
+        };
     }
 }
