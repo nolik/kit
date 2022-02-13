ec2dd7533f4d6623e96c4b87893bfaa9d7923afc
Ihar Novik <i.novik@godeltech.com>
2022-02-13 23:26:18 +0300

    api-refactoring - remove redundant headers.

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ use rdkafka::config::{ClientConfig, RDKafkaLogLevel};
 use rdkafka::consumer::stream_consumer::StreamConsumer;
 use rdkafka::consumer::{CommitMode, Consumer, ConsumerContext, Rebalance};
 use rdkafka::error::KafkaResult;
-use rdkafka::message::{Headers, Message};
+use rdkafka::message::Message;
 use rdkafka::topic_partition_list::TopicPartitionList;
 
 #[tokio::main]
